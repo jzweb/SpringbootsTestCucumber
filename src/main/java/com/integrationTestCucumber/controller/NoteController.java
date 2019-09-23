745cd2aa88ed2d0a0f934fd13cc8f0b006502b0e
Jorge Zevallos <jorgezevallos@Jorges-MacBook-Pro.local>
2019-09-22 22:03:40 -0500

    add assured 2

--- src/main/java/com/integrationTestCucumber/controller/NoteController.java
+++ src/main/java/com/integrationTestCucumber/controller/NoteController.java
@@ -1,5 +1,23 @@
 package com.integrationTestCucumber.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.integrationTestCucumber.model.Note;
+
+@RestController
 public class NoteController {
+	
+	@RequestMapping(value="agregar/nota",method=RequestMethod.GET)
+	public ResponseEntity<Object> addNote(@RequestBody Note note){
+		
+		return new ResponseEntity<Object>(null, HttpStatus.CREATED);
+		
+	}
 
 }
